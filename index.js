e195ae4cdd034576c97d25ab02b0d9301f3b1ce8
rpawar <rpawar@innominds.com>
2023-08-29 18:03:42 +0530

    tutorial on req and res in node js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('hello world')
+console.log('hello world');
+const http = require('http');
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write('<html><form action="/message" method="POST"><label> <input type="text" name="message"/></label><button type="submit">Submit</button></form></html>')
+        return res.end();
+    }
+    if (req.url === '/message') {
+        const body = []
+        req.on('data', (chunk) => {
+            console.log(chunk, 'chunk...')
+            body.push(chunk)
+        })
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1]
+            console.log(message)
+        })
+        res.statusCode = 302;
+        res.setHeader('Location', '/')
+        return res.end();
+    }
+    res.write('<html><p>Hello world</p></html>')
+    res.end();
+})
+
+
+server.listen('8080')
