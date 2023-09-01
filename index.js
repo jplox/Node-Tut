30353dab4949ee71dda9dc5dc87b8d4ad5968fc0
rpawar <rpawar@innominds.com>
2023-09-01 13:04:38 +0530

    made separate routes

--- index.js
+++ index.js
@@ -1,30 +1,6 @@
 console.log('hello world');
 const http = require('http');
-const fs = require('fs')
+const requestHandler = require('./routes')
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.write('<html><form action="/message" method="POST"><label> <input type="text" name="message"/></label><button type="submit">Submit</button></form></html>')
-        return res.end();
-    }
-    if (req.url === '/message') {
-        const body = []
-        req.on('data', (chunk) => {
-            console.log(chunk, 'chunk...')
-            body.push(chunk)
-        })
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1]
-            console.log(message)
-        })
-        res.statusCode = 302;
-        res.setHeader('Location', '/')
-        return res.end();
-    }
-    res.write('<html><p>Hello world</p></html>')
-    res.end();
-})
-
-
+const server = http.createServer(requestHandler)
 server.listen('8080')
